c010c79c19d20918879cbc726c3f24f622f3c2de
Elea641 <eleavoliotis64@gmail.com>
2024-06-05 14:02:57 +0200

    type

--- client/src/pages/PokemonDetails.jsx
+++ client/src/pages/PokemonDetails.jsx
@@ -1,14 +1,21 @@
 import { Link, useLoaderData } from "react-router-dom";
-import PokemonCard from "../components/PokemonCard";
 
 function PokemonDetails() {
   const pokemon = useLoaderData();
+  const types = pokemon.types.split(",");
   return (
     <>
       <Link to={`/pokemon/edition/${pokemon.id}`}>
         <h2>EDIT</h2>
       </Link>
-      <PokemonCard pokemon={pokemon} />
+      {/* <PokemonCard pokemon={pokemon} /> */}
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.image_url} alt={pokemon.name} />
+      {types.map((type) => (
+        <p key={type.id} value={type.name}>
+          {type}
+        </p>
+      ))}
     </>
   );
 }
