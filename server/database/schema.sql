490570045b5be7071cab123265703fb422946389
Elea641 <eleavoliotis64@gmail.com>
2024-06-06 08:10:33 +0200

    add database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,11 +1,17 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
-);
-
 create table pokemon (
   id int unsigned primary key auto_increment not null,
   name varchar(255) not null,
   image_url varchar(255) not null
 );
+
+INSERT INTO pokemon (name, image_url) VALUES
+('Bulbasaur', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'),
+('Charmander', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'),
+('Squirtle', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'),
+('Pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'),
+('Jigglypuff', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png'),
+('Meowth', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png'),
+('Psyduck', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png'),
+('Machop', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/066.png'),
+('Gengar', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png'),
+('Eevee', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png');
