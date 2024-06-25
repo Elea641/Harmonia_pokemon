86eb6632c65db53a20a3b6c421636159495d34df
Elea641 <eleavoliotis64@gmail.com>
2024-06-25 14:44:00 +0200

    tests

---
+++ server/tests/pokemons/repository.spec.js
@@ -0,0 +1,45 @@
+// Import required dependencies
+const { database, tables } = require("../config");
+
+// Import repository classes
+const AbstractRepository = require("../../database/models/AbstractRepository");
+const PokemonRepository = require("../../database/models/PokemonRepository");
+
+describe("ItemRepository", () => {
+  test("ItemRepository extends AbstractRepository", async () => {
+    expect(Object.getPrototypeOf(PokemonRepository)).toBe(AbstractRepository);
+  });
+
+  test("tables.pokemon = new PokemonRepository", async () => {
+    expect(tables.pokemon instanceof PokemonRepository).toBe(true);
+  });
+
+  test("readAll => select", async () => {
+    const rows = [];
+
+    jest.spyOn(database, "query").mockImplementation(() => [rows]);
+
+    const returned = await tables.pokemon.readAll();
+
+    expect(database.query).toHaveBeenCalledWith("select * from pokemon");
+    expect(returned).toStrictEqual(rows);
+  });
+
+  test("read => select with id", async () => {
+    // Mock rows returned from the database
+    const rows = [{}];
+
+    // Mock the implementation of the database query method
+    jest.spyOn(database, "query").mockImplementation(() => [rows]);
+
+    // Call the read method of the item repository
+    const returned = await tables.pokemon.read(0);
+
+    // Assertions
+    expect(database.query).toHaveBeenCalledWith(
+      "select * from pokemon where id = ?",
+      [0]
+    );
+    expect(returned).toStrictEqual(rows[0]);
+  });
+});
---
+++ server/tests/pokemons/routes.spec.js
@@ -0,0 +1,15 @@
+// Import required dependencies
+const { app, request, database } = require("../config");
+
+describe("GET /api/pokemons", () => {
+  it("should fetch items successfully", async () => {
+    const rows = [];
+
+    jest.spyOn(database, "query").mockImplementation(() => [rows]);
+
+    const response = await request(app).get("/api/pokemons");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual(rows);
+  });
+});
