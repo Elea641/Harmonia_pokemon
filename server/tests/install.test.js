9e2177f26f1c6d94ebb20b9e34412552733184dd
Elea641 <eleavoliotis64@gmail.com>
2024-06-26 13:31:15 +0200

    test fixe

--- server/tests/install.test.js
+++ server/tests/install.test.js
@@ -12,22 +12,4 @@ describe("Installation", () => {
   test("You have retained /server/.env.sample", async () => {
     expect(fs.existsSync(path.join(__dirname, "..", ".env.sample"))).toBe(true);
   });
-
-  // Test: Check if the .env file is properly filled with valid database connection information
-  // test("You have filled /server/.env with valid information to connect to your database", async () => {
-  //   // Query the database to check if the connection is successful
-  //   const [rows] = await database.query(`select 1`);
-
-  //   // Expecting at least one row to be returned, indicating a successful connection
-  //   expect(rows.length).toBeGreaterThan(0);
-  // });
-
-  // // Test: Check if the database migration and seeding scripts have been executed
-  // test("You have executed the db:migrate and db:seed scripts", async () => {
-  //   // Query the 'item' table to check if any data has been inserted
-  //   const [rows] = await database.query(`select * from item`);
-
-  //   // Expecting at least one row to be returned, indicating successful migration and seeding
-  //   expect(rows.length).toBeGreaterThan(0);
-  // });
 });
--- server/tests/pokemons/routes.spec.js
+++ server/tests/pokemons/routes.spec.js
@@ -1,5 +1,5 @@
 // Import required dependencies
-const { app, request, database } = require("../config");
+const { app, request } = require("../config");
 const pokemonActions = require("../../app/controllers/pokemonActions");
 
 describe("GET /api/pokemons", () => {
@@ -9,14 +9,17 @@ describe("GET /api/pokemons", () => {
   test("your browse function has 3 parameters: req, res and next", async () => {
     expect(pokemonActions.browse).toHaveLength(3);
   });
-  test("should fetch items successfully", async () => {
-    const rows = [];
+});
 
-    jest.spyOn(database, "query").mockImplementation(() => [rows]);
-
-    const response = await request(app).get("/api/pokemons");
-
-    expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual(rows);
+describe("GET /api/pokemons", () => {
+  it("responds with json", (done) => {
+    request(app)
+      .get("/api/pokemons")
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        console.warn("Response in describe block:", res.body);
+        return done();
+      });
   });
 });
